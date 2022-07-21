13d95e42fad50db938c034d17ff7db4c217a5c72
Kamil Szczygiel <distortec@distortec.com>
2022-07-21 12:51:53 +0200

    Use <cassert> instead of "distortos/assert.h"

--- mbedTlsThreading.cpp
+++ mbedTlsThreading.cpp
@@ -2,7 +2,7 @@
  * \file
  * \brief Definitions of threading-related functions for mbed TLS
  *
- * \author Copyright (C) 2019 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
+ * \author Copyright (C) 2019-2022 Kamil Szczygiel http://www.distortec.com http://www.freddiechopin.info
  *
  * \par License
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
@@ -11,8 +11,9 @@
 
 #include "mbedtls/threading.h"
 
-#include "distortos/assert.h"
 #include "distortos/BIND_LOW_LEVEL_INITIALIZER.h"
+
+#include <cassert>
 
 namespace
 {
